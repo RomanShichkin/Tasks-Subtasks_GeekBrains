1b097fdbf0077138b4492e12945e313a34aee84b
RomanShichkin <oman01_93@mail.ruoman01_93@mail.ruroman01_93@mail.ru>
2021-08-26 16:18:16 +0300

    Практическое задание к уроку 5

---
+++ Tasks&Subtasks_GeekBrains/Images.swift
@@ -0,0 +1,16 @@
+//
+//  Images.swift
+//  Tasks&Subtasks_GeekBrains
+//
+//  Created by Роман Шичкин on 25.08.2021.
+//
+
+import UIKit
+
+// MARK: - Flyweight Images
+
+extension UIImage {
+    static let circle = UIImage(systemName: "circle")
+    static let circleFill = UIImage(systemName: "circle.fill")
+    static let plus = UIImage(systemName: "plus")
+}
